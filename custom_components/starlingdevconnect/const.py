b525481e3893a1dc39ec84993e7f33de0658e409
Thomas Lomas <tlomas@gmail.com>
2024-02-03 23:18:51 +0000

    rename

--- custom_components/starlingdevconnect/const.py
+++ custom_components/starlingdevconnect/const.py
@@ -1,9 +1,9 @@
-"""Constants for integration_blueprint."""
+"""Constants for starlingdevconnect."""
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
 NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
+DOMAIN = "starlingdevconnect"
 VERSION = "0.0.0"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
